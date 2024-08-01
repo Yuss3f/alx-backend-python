27fafe62400da4e0c54cea973254dfe46355a01b
Yuss3f <kayke8youssef@gmail.com>
2024-08-01 09:34:32 +0000

    py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Task 2's module: Basic annotations - floor
+'''
+
+
+import math
+
+
+def floor(a: float) -> int:
+    '''Computing the floor of a floating-point number.
+    '''
+    return math.floor(a)
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+This is the "0x00-python_variable_annotations" by Yuss3f
